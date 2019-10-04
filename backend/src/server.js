824bc9a28a6448939c21dc9f1a01a5a7c5fce78b
Marcos Warmling Berti <marcos_wb@outlook.com>
2019-10-04 19:02:41 -0300

    comunicação em tempo real com socket io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
 
+const socketio = require('socket-io')
+const http = require('http')
+
 const routes = require('./routes')
 
 const POR_LISTENING = 3333
@@ -10,10 +13,27 @@ const DB_USER = 'omnistack'
 const DB_PASSWD = 'omnistack'
 
 const app = express()
+const server = http.Server(app)
+const io = socketio(server)
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWD}@omnistack-d2btd.mongodb.net/semana09?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+})
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query
+
+    connectedUsers[user_id] = socket.id
+})
+
+app.use((req, res, next) => {
+    req.io = io
+    req.connectedUsers = connectedUsers
+
+    return next()
 })
 
 app.use(express.json())
@@ -21,4 +41,4 @@ app.use(cors)
 app.use('/files', express.static(path.resolve(__dirname, '..', 'upload')))
 app.use(routes)
 
-app.listen(POR_LISTENING)
+server.listen(POR_LISTENING)
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
+    // mudar em pages/List também
     baseURL: 'http://192.168.0.102:3333'
 })
 
